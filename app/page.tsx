62a992125bfd7e63724a1c1bd11d1574181583ab
iFabruce <fabrucemaminiaina@gmail.com>
2023-12-27 01:14:19 +0300

    fix logo

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
         <nav className=" border-gray-200 dark:bg-gray-900">
           <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto py-6">
             <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
-                <Image src="/images/Logo.png" className="" width={128} height={128} alt="Unfiltered Athletes Logo" />
+                <Image src="/images/logo.png" className="" width={128} height={128} alt="Unfiltered Athletes Logo" />
             </a>
             <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-start p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
                 <span className="sr-only">Open main menu</span>
